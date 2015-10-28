44583b0c3520dab815edee3cb63b630831e64236
Jakob Borg <jakob@nym.se>
2015-10-28 11:15:09 +0100

    Support subpaths and match expressions

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"regexp"
 	"sync"
 )
 
@@ -16,14 +18,21 @@ type api struct {
 	Building  bool
 	Result    string
 	Artifacts []struct {
-		FileName string
+		FileName     string
+		RelativePath string
 	}
 	URL string
 }
 
 func main() {
+	expStr := flag.String("match", "", "Regexp to match file names against")
 	flag.Parse()
 	url := flag.Arg(0)
+
+	var exp *regexp.Regexp
+	if *expStr != "" {
+		exp = regexp.MustCompile(*expStr)
+	}
 
 	resp, err := http.Get(url + "/api/json")
 	if err != nil {
@@ -43,9 +52,13 @@ func main() {
 	var wg sync.WaitGroup
 	for _, art := range res.Artifacts {
 		file := art.FileName
+		relPath := art.RelativePath
+		if exp != nil && !exp.MatchString(file) {
+			continue
+		}
 		wg.Add(1)
 		go func() {
-			if err := download(res.URL, file); err != nil {
+			if err := download(res.URL, relPath); err != nil {
 				log.Println("Download of", file, "failed:", err)
 			}
 			wg.Done()
@@ -55,7 +68,8 @@ func main() {
 }
 
 func download(url, file string) error {
-	log.Println("Downloading", file)
+	short := filepath.Base(file)
+	log.Println("Downloading", short)
 
 	path := url + "/artifact/" + file
 	resp, err := http.Get(path)
@@ -67,7 +81,7 @@ func download(url, file string) error {
 		return errors.New(resp.Status)
 	}
 
-	fd, err := os.Create(file)
+	fd, err := os.Create(short)
 	if err != nil {
 		return err
 	}
